dd7f4d923b3753d739baadd7d8134ecb13d93973
CamiloTangoa <89042732+CamiloTangoa@users.noreply.github.com>
2021-09-29 14:46:43 -0500

    Vue

---
+++ vue-ecommerce/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ff90525eedc3aea7b3428cdab75280d'),
+    },
+  },
+});
